b7d636514de20c4cfac1186d91ba8807bbc51d8f
unknown <perviz.elizade.2002@mail.ru>
2022-05-30 22:28:56 +0400

    first commit

--- ConstrainTaskk.sql
+++ ConstrainTaskk.sql
@@ -4,11 +4,12 @@
 
  Create Table Employees(
  Id INT ,
- FullName nvarchar(30) NOT NULL,CHECK(Len(Fullname)>3) ,
+ FullName nvarchar(30) NOT NULL,
  Salary INT CHECK(salary>0),
  DepartmentId INT,
  Email nvarchar(50) NOT NULL,
  CONSTRAINT pk_Employees PRIMARY KEY (Id),
+ CONSTRAINT ck_Salary CHECK (Salary>=0),
  CONSTRAINT fk_Employees_Id FOREIGN KEY(DepartmentId) REFERENCES Departments(Id),
  CONSTRAINT uk_Email UNIQUE(Email)
  )
@@ -17,6 +18,7 @@
  Id INT,
  Name nvarchar(30) NOT NULL,
  CONSTRAINT pk_Departments PRIMARY KEY(Id),
- CONSTRAINT uk_Departments UNIQUE (Name)
+ CONSTRAINT ck_Departmets_Name CHECK(LEN(Name)>3)
  )
+
  
